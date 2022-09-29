2519e4ac97e3561896219ba3ac4af9227f167185
Paige <paige1.johnson@umconnect.umt.edu>
2022-09-29 14:54:00 -0600

    Heroku Updates

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x+r_wb2cny7q_k2se+a!qri3!u#r!a1b&d=d0^n_l5&$*m)35s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
